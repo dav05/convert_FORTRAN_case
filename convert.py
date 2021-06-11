70f939304a04161170f523b74f675aa7f7310476
dav05 <47261801+dav05@users.noreply.github.com>
2021-06-12 00:11:40 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 # covert.py
 # python3 script for case conversion of source code files written in fixed form fortran.
 #
- #python3 script for case conversion of source code files written in
+# python3 script for case conversion of source code files written in
 # fixed form fortran. Ignores comments, strings, escape
 # sequences within strings and some special words.
 #
